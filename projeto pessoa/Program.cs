108387dec6497e2a3e98c2e2f2c51e54b29ff148
Lucas Prates <100208473+pratestech@users.noreply.github.com>
2024-06-28 17:16:59 -0300

    Add files via upload

---
+++ projeto pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        pessoa p1 = new pessoa("GODOFREDO",18);
+        //p1.AlteraNome("Kaike");
+        //p1.AlteraSobreNome("Pinheiro Simplicio");
+        //p1.AlteraIdade(15);
+        p1.MostraDados();
+    }
+}
---
+++ projeto_loja/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace projeto_loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+       //produto p1 = new produto();
+       //produto p1 = new produto("XYZ");
+       Produto p1 = new Produto("ABC",10);
+       p1.MostraDados();
+    }
+}

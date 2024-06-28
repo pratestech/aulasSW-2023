bc051b895b6c9cc55de3c9a29543510d3cb3e05b
Lucas Prates <100208473+pratestech@users.noreply.github.com>
2024-06-28 17:10:26 -0300

    Add files via upload

---
+++ Aeroporto/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aeroporto;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aeroporto a1 = new Aeroporto();
+        a1.AlteraCodigo(1);
+        a1.AlteraNome("MCM Airlines");
+        a1.AlteraEntrada("São Paulo");
+        a1.AlteraSaida("Orlando");
+        a1.MostraDados();
+
+    }
+}
